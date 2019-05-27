3b7fd1deff9a67bb4a59b82d8f9262816acfb225
Jake <git@ja-ke.tech>
2019-05-27 21:25:30 +0200

    Add classic Neopixel lib

---
+++ src/hw.h
@@ -0,0 +1,9 @@
+// Connect theWS2812B LED strip here
+#define LED_PIN 12
+
+#define LED_COUNT 100
+#define LED_START 0
+#define LED_END 99
+
+// Osci Trigger Pin, for performance measurements
+#define TRIG_PIN 14
